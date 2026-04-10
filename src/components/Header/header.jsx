9b6c0ab21af2a546308d63096907e2f4d18dbf41
Ykan <dddfff4481@gmail.com>
2026-04-10 07:56:24 +0300

    fix v3

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -104,7 +104,7 @@ export default function Header({ setIsLoginOpen }) {
                             </>
                         )}
 
-                       
+
                     </ul>
                 </div>
             </div>
