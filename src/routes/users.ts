3c688245332d8c999c8ceb830fc44da14c2469d1
Michael R-V <lpsh4ot@gmail.com>
2026-04-12 20:59:52 +0300

    1.2.5.3 Tweaked server.ts CORS works in prod thirteen times

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -158,7 +158,7 @@ module.exports = async (instance: FastifyInstance) => {
     return res
       .setCookie("access_token", token, {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: "none",
         signed: true,
         path: "/",
@@ -185,7 +185,7 @@ module.exports = async (instance: FastifyInstance) => {
     return res
       .setCookie("access_token", token, {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: "lax",
         signed: true,
         path: "/",
@@ -205,7 +205,7 @@ module.exports = async (instance: FastifyInstance) => {
     res
       .clearCookie("access_token", {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: "none",
         signed: true,
         path: "/",
