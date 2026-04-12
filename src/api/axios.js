a47722cf8507824281936e016f71f96b492989da
Michael R-V <lpsh4ot@gmail.com>
2026-04-12 20:45:42 +0300

    10 commits to tweak for deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://api.ryban.ru/api",
-    withCredentials: true // Required because you use Cookies/JWT
+    baseURL: "http://89.109.16.50:911/api",
+    withCredentials: true
 });
