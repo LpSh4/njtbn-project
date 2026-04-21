39b895fb82323e8971d350e64d993f5a0d0237e9
Michael R-V <lpsh4ot@gmail.com>
2026-04-21 12:51:13 +0300

    Tinkering with nginx

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://ryban.ru/api",
+    baseURL: "https://api.ryban.ru/api",
 
     withCredentials: true
 });
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://api.domain.ru',
+        target: 'https://api.ryban.ru',
         changeOrigin: true,
         secure: true,
 
