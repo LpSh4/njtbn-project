01e41ffe2cf6f47569b0ad817921168da9c68b31
Ykan <dddfff4481@gmail.com>
2026-04-21 23:41:11 +0300

    fix auth

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://api.ryban.ru/api",
+
+    baseURL: "/api",
 
     withCredentials: true
 });
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [
+    react(),
+    basicSsl()
+  ],
   server: {
-    port: 3000,
+    https: true,
     proxy: {
       '/api': {
         target: 'https://api.ryban.ru',
         changeOrigin: true,
-        secure: true,
-
-      }
-    }
-  }
+      },
+    },
+  },
 })
