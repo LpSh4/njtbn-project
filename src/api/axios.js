45cea8f95e21016c493cf23aca6d0f1144ea09ef
Michael R-V <lpsh4ot@gmail.com>
2026-05-09 14:47:51 +0300

    AXIOS fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-   baseURL: "/api",
-   withCredentials: true
+    baseURL: "https://api.ryban.ru/api",
+
+
+    withCredentials: true
 });
